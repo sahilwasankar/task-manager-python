2cd01a1fac82d7f059bc5bb9cfa915cd08bb1cf2
sahilwasankar <sahilwasankar777@gmail.com>
2026-01-25 14:05:38 +0530

    feat: add TaskManager class and fix Task class issues
    - Added TaskManager with add_task and view_tasks functionality
    - Prevents duplicate task IDs when adding tasks
    - Displays all tasks with correct status (Done/Pending)
    - Fixed Task.from_csv_row to return Task object instead of tuple
    - Ensures CSV conversion works correctly for further execution

--- task.py
+++ task.py
@@ -1,20 +1,20 @@
 class Task :
-    def __init__(self, task_id,title,complete = False):
+    def __init__(self, task_id,title,completed = False):
 
         self.task_id = task_id
         self.title = title
-        self.complete = complete
+        self.completed = completed
 
-    def mark_complete(self):
-     self.complete = True
+    def mark_completed(self):
+     self.completed = True
 
     def to_csv_row(self):
-        return[self.task_id,self.title,self.status]
+        return[self.task_id,self.title,self.completed]
     
     @staticmethod
-    def form_csv_row(row):
+    def from_csv_row(row):
        task_id = int(row[0])
        title = row[1]
-       complete = row[2] == "True"
-       return(task_id,title,complete)
+       completed = row[2] == "True"
+       return Task(task_id,title,completed)
        
---
+++ task_manager.py
@@ -0,0 +1,21 @@
+from task import Task
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self,task):
+        for t in self.tasks :
+            if t.task_id == task.task_id:
+                print("Task with this ID already exists")
+                return
+            
+        self.tasks.append(task) 
+        print("Task added Successfully")       
+
+    def view_tasks(self):
+        if not self.tasks :
+            print("Task not found")
+
+        for task in self.tasks:
+              status = "Done" if task.completed else "Pending"
+              print(f"ID: {task.task_id} Title: {task.title} Status: {status}")
